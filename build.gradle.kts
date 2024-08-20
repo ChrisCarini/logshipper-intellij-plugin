209fe12cbb07e8cfcc6dac2123b15d34cf564603
Chris Carini <6374067+ChrisCarini@users.noreply.github.com>
2024-08-20 04:52:22 -0700

    Upgrading IntelliJ from 2024.1.5 to 2024.2.0 (#348)

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("build-standard-jetbrains-plugin-build")
+}
+
+dependencies {
+    implementation("javax.json:javax.json-api:1.1.4")
+    implementation("org.glassfish:javax.json:1.1.4")
+}
